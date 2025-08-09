d814c2495209af872c3400c279c8d1116e45af96
Sahil Hinge <sahilhinge89@gmail.com>
2025-08-09 11:43:58 +0530

    Basic changes

--- config/database.js
+++ config/database.js
@@ -1,11 +1,14 @@
 const mongoose  = require('mongoose');
 require('dotenv').config();
-mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}) .then(() => {
-    console.log("Database connected successfully");
-}).catch((error) => {
-    console.error("Database connection error:", error);
-    process.exit(1);
-});
+exports.connect =()=>{
+
+    mongoose.connect(process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }) .then(() => {
+        console.log("Database connected successfully");
+    }).catch((error) => {
+        console.error("Database connection error:", error);
+        process.exit(1);
+    });
+}
