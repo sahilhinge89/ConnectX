cf11f5bc4dfc5b231e0566aaf6d9b7668af6534e
Sahil Hinge <sahilhinge89@gmail.com>
2025-08-11 00:11:08 +0530

    connect server

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const app = express();
+
+const userRoutes = require("./routes/User");
+const profileRoutes = require("./routes/Profile");
+const paymentRoutes = require("./routes/Payment");
+const courseRoutes = require("./routes/Course");
+const database = require('./config/database');
+const cookieeParser = require('cookie-parser');
+const cors = require('cors');
+const {cloudinaryConnect} = require('./config/cloudinary');
+const fileUpload = require("express-fileupload");
+ 
+dotenv.config();
+const PORT = process.env.PORT || 4000;
+// database connection
+database.connect();
+//middleware
+app.use(express.json());
+app.use(cookieeParser());
+
+// connection between frontend and backend
+app.use(
+    cors({
+        origin:"http://localhost:3000",
+        credentials:true,
+    })
+)
+app.use(
+    fileUpload({
+        useTempFiles:true,
+        tempFileDir:"/tmp",
+    })
+)
+
+//cloudinary connection
+cloudinaryConnect();
+// routes
+app.use ("/api/v1/auth",userRoutes);
+app.use ("/api/v1/profile",profileRoutes);
+app.use("/api/v1/payments",paymentRoutes);
+app.use ("/api/v1/course",courseRoutes);
+
+//default route
+
+app.get("/",(req,res)=>{
+    return res.json({
+        success:true,
+        message: 'Your server is up and running....'
+    })
+})
+app.listen(PORT,()=>{
+    console.log("App is running at $ {PORT}")
+})
